987140b61d24832364a5591d702239f16976fdea
rob-murray <robmurray17@gmail.com>
2013-01-16 15:42:11 +0000

    Added Common.h with Common API key details

---
+++ OSTileSourceDemo/OSTileSourceDemo/Common.h
@@ -0,0 +1,22 @@
+//
+//  Common.h
+//  OSTileSourceDemo
+//
+//  Created by rmurray on 16/01/2013.
+//  Copyright (c) 2013 OrdnanceSurvey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+/*
+ * Define your OS Openspace API KEY details
+ * @see http://www.ordnancesurvey.co.uk/oswebsite/web-services/os-openspace/index.html
+ */
+extern NSString *const kOSApiKey;
+extern NSString *const kOSApiKeyUrl;
+extern BOOL const kOSIsPro;
+
+
+@end
---
+++ OSTileSourceDemo/OSTileSourceDemo/Common.m
@@ -0,0 +1,22 @@
+//
+//  Common.m
+//  OSTileSourceDemo
+//
+//  Created by rmurray on 16/01/2013.
+//  Copyright (c) 2013 OrdnanceSurvey. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+/*
+ * Define your OS Openspace API KEY details below
+ * @see http://www.ordnancesurvey.co.uk/oswebsite/web-services/os-openspace/index.html
+ */
+NSString *const kOSApiKey = @"YOUR_API_KEY";
+NSString *const kOSApiKeyUrl = @"URL_HERE";
+const BOOL kOSIsPro = YES;
+
+
+@end
