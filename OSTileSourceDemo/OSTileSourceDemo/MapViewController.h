89e57759014d068a30bb565c5652b4db3f609ed3
rob-murray <robmurray17@gmail.com>
2013-04-16 16:57:20 +0100

    Add SystemConfig framework

---
+++ OSTileSourceDemo/OSTileSourceDemo/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  OSTileSourceDemo
+//
+//  Created by rmurray on 16/04/2013.
+//  Copyright (c) 2013 OrdnanceSurvey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ OSTileSourceDemo/OSTileSourceDemo/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  OSTileSourceDemo
+//
+//  Created by rmurray on 16/04/2013.
+//  Copyright (c) 2013 OrdnanceSurvey. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
